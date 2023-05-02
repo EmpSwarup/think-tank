b291355524ef5dc4cec47e717ff3286209d0b666
Nishant chaudhary <nishantchaudhary842@gmail.com>
2023-05-02 19:31:57 +0545

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
         <span className="heart">
           <AiFillHeart />{" "}
         </span>{" "}
-        by EmpSwarup
+        by Nishant
       </h3>
     </div>
   );
--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,6 +1,7 @@
 // import React from "react";
 import "./HomeContainer.css";
 import "animate.css";
+import data from "./data.json";
 
 const HomeContainer = () => {
 
@@ -72,6 +73,7 @@ const HomeContainer = () => {
       </div>
       <div className="ag-format-container animate__animated animate__fadeIn animate__delay-1s">
         <div className="ag-courses_box">
+<<<<<<< HEAD
           {
 
             subjects.map((item) => {
@@ -93,6 +95,22 @@ const HomeContainer = () => {
             })
 
           }
+=======
+          {data.semesters.map((item) => {
+            return (
+              <div key={item.id} className="ag-courses_item">
+                <a href="#" className="ag-courses-item_link">
+                  <div className="ag-courses-item_bg" />
+                  <div className="ag-courses-item_title">{item.sem}</div>
+                  <div className="ag-courses-item_date-box">
+                    Subjects:<span> </span>
+                    <span className="ag-courses-item_date">{item?.sub}</span>
+                  </div>
+                </a>
+              </div>
+            );
+          })}
+>>>>>>> 22355c9f9912b851d9011d9b54f0b68639a6470f
         </div>
       </div>
     </>
