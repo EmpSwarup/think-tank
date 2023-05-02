4b0dac5265f120bdf62bd408eb28980c96ac5242
Nishant chaudhary <nishantchaudhary842@gmail.com>
2023-05-02 21:08:52 +0545

    remove table

--- src/pages/respectivePage.jsx
+++ src/pages/respectivePage.jsx
@@ -6,25 +6,16 @@ const RespectivePage = () => {
   console.log(state)
   return (
     <Layout>
-      <table>
-        <tr>
-          <th>Subjects</th>
-          <th>Credit</th>
-          <th>Notes</th>
-        </tr>
+ 
         {
           state.subjects.map((item, id) => {
             return (
-              <div key={id}>
-                <tr>
-                  <td>{item}</td>
-                </tr>
-
+              <div key={id}> 
+             <li>{item}</li>
               </div>
             )
           })
         }
-      </table>
     </Layout>
   )
 }
